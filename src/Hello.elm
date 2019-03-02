231f77bf10e0c8905d3425782e6a2aaa6960a0ec
hhiroki <h-hiroki@users.noreply.github.com>
2019-03-02 09:36:45 +0900

    ⛳️リンク集にした

--- src/Hello.elm
+++ src/Hello.elm
@@ -1,6 +1,18 @@
-import Html exposing (Html, a, text)
+import Html exposing (Html, a, text, div, h1, ul, li)
 import Html.Attributes exposing (href)
 
 main : Html msg
 main =
-    a [ href "https://elm-lang.org" ] [ text "Elm" ]
+    div [] [ header, content ]
+
+header : Html msg
+header =
+    h1 [] [ text "Useful links" ]
+
+content : Html msg
+content =
+    ul []
+        [ li [] [ a [ href "https://elm-lang.org" ] [ text "HomePage" ] ]
+        , li [] [ a [ href "https://package.elm-lang.org" ] [ text "Packages" ] ]
+        , li [] [ a [ href "https://ellie-app.com" ] [ text "Playground" ] ]
+        ]
