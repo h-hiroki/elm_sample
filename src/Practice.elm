7d29569056318a71cdd19da2789a451d8b81ed24
hhiroki <h-hiroki@users.noreply.github.com>
2019-03-03 13:43:03 +0900

    ⛳️Elmアーキテクチャの雛形を作成

---
+++ src/Practice.elm
@@ -0,0 +1,36 @@
+import Browser
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+
+main : Program () Model Msg
+main =
+    Browser.sandbox
+        { init = init
+        , update = update
+        , view = view
+        }
+
+
+-- MODEL
+type alias Model =
+    {}
+
+init : Model
+init =
+    {}
+
+
+-- UPDATE
+type Msg =
+    Msg
+
+update : Msg -> Model -> Model
+update msg model =
+    model
+
+
+-- VIEW
+view : Model -> Html Msg
+view model =
+    text ""
